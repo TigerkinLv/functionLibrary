2786171ab1a89ea79255e2aa8bd295228c402d12
Liwei <5Gavin_Liwei>
2018-12-03 17:59:33 +0800

    add rotateImage in sundry

--- sundry.js
+++ sundry.js
@@ -54,3 +54,74 @@
  }
 
 
+/**
+ * 旋转一个图片并返回新的图片对象
+ * @param {*} pid 要图片的id
+ * @param {*} direction 旋转的方向
+ */
+ function rotateImg(pid,direction){
+    // 最大和最小旋转方向，图片旋转4次回到原方向
+    var min_step=0;
+    var max_step=3;
+    var img=document.getElementById(pid);
+    if(img==null){
+        return;
+    }
+    var height=img.height;
+    var width=img.width;
+    var step=img.getAttribute("step");
+    if(step==null){
+        step=min_step;
+    }
+    if(direction=="right"){
+        step++;
+        step>max_step && (step=min_step);
+    }else{
+        step--;
+        step<min_step && (step=max_step);
+    }
+    img.setAttribute("step",step);
+    var canvas=document.getElementById("pic_"+pid);
+    if(canvas==null){
+        canvas=document.createElement("canvas");
+        canvas.setAttribute("id","pic_"+pid);
+    }
+    // 旋转的角度以弧度值为参数
+    var degree= step*90*Math.PI/180;
+    var ctx= canvas.getContext("2d");
+    switch(step){
+        case 0:
+            canvas.width=width;
+            canvas.height=height;
+            ctx.drawImage(img,0,0);
+            break;
+        case 1:
+            canvas.width=height;
+            canvas.height=width;
+            ctx.rotate(degree);
+            ctx.drawImage(img,0,-height);
+            break;
+        case 2:
+            canvas.width=width;
+            canvas.height=height;
+            ctx.rotate(degree);
+            ctx.drawImage(img,-width,-height);
+            break;
+        case 3:
+            canvas.width=height;
+            canvas.height=width;
+            ctx.rotate(degree);
+            ctx.drawImage(img,width,0);
+            break;
+    }   
+    return canvas2Img(canvas);
+ }
+/**
+ * canvas 转图片
+ * @param {*} canvas 要转的canvas
+ */
+ function canvas2Img(canvas){
+     var image=new  Image();
+     image.src=canvas.toDataURL("image/png");
+     return image;
+ }
